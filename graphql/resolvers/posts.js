d9d33af3826128736ae54582865d57b91fa3b91f
genesisschaerrer <genesisschaerrer.dev@gmail.com>
2021-02-16 14:45:57 -0700

    added ability to delete post

--- graphql/resolvers/posts.js
+++ graphql/resolvers/posts.js
@@ -1,3 +1,5 @@
+const {AuthenticationError} = require("apollo-server")
+
 const Post = require("../../models/post")
 const checkAuth = require("../../utils/check-auth")
 
@@ -5,7 +7,7 @@ module.exports = {
     Query: {
         async getPosts(){
             try{
-                const posts = await Post.find()
+                const posts = await Post.find().sort({ createdAt: -1 })
                 return posts
             } catch (err){
                   throw new Error(err)
@@ -34,11 +36,26 @@ module.exports = {
                 body,
                 user: user.id,
                 username: user.username,
-                createdAt: new Date().toISOString 
+                createdAt: new Date().toISOString() 
             })
 
             const post = await newPost.save()
             return post
+        }, 
+        async deletePost(_, {postId}, context){
+            const user = checkAuth(context)
+
+            try{
+                const post = await Post.findById(postId)
+                if(user.username === post.username){
+                    await post.delete()
+                    return "Post deleted"
+                } else {
+                    throw new AuthenticationError("action not allowed")
+                }
+            } catch(error){
+                throw new Error(error)
+            }
         }
     }
 }
